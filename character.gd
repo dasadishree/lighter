d6a939396a169c7682df4ef39cb2015797272734
dasadishree <dasadishree@gmail.com>
2026-01-31 22:44:46 -0500

    interactiosn

--- character.gd
+++ character.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 12
 
+var xform : Transform3D
+
 func _physics_process(delta: float) -> void:
 #	rotate the camera left & right
 	if Input.is_action_just_pressed("cam_left"):
@@ -26,8 +28,17 @@ func _physics_process(delta: float) -> void:
 	
 #	Rotate the character mesh so oriented towards the direction moving in relation to the camera
 	if input_dir != Vector2(0,0):
-		$MeshInstance3D.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) -90
+		$MeshInstance3D.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) +90
 
+#	Rotate the character to align with floor
+	if is_on_floor():
+		align_with_floor($RayCast3D.get_collision_normal())
+		global_transform = global_transform.interpolate_with(xform, 0.3)
+	elif not is_on_floor():
+		align_with_floor(Vector3.UP)
+		global_transform = global_transform.interpolate_with(xform, 0.3)
+	
+# 	Update the velocity and move the character
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -38,3 +49,13 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	#make camera move w character
 	$Camera_Controller.position = lerp($Camera_Controller.position, position, 0.15)
+
+func align_with_floor(floor_normal):
+		xform = global_transform
+		xform.basis.y = floor_normal
+		xform.basis.x = xform.basis.z.cross(floor_normal)
+		xform.basis = xform.basis.orthonormalized()
+
+
+func _on_fall_zone_body_entered(body: Node3D) -> void:
+	get_tree().change_scene_to_file("res://level_1.tscn")
