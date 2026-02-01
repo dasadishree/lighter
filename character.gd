6944382595a06c55732b138a0e4c3d4c1408cae4
dasadishree <dasadishree@gmail.com>
2026-01-31 21:19:11 -0500

    camera move angle

--- character.gd
+++ character.gd
@@ -1,12 +1,15 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
+const JUMP_VELOCITY = 12
 
 func _physics_process(delta: float) -> void:
-	
+#	rotate the camera left & right
+	if Input.is_action_just_pressed("cam_left"):
+		$Camera_Controller.rotate_y(deg_to_rad(30))
+	if Input.is_action_just_pressed("cam_right"):
+		$Camera_Controller.rotate_y(deg_to_rad(30))
+
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -16,9 +19,15 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+#	New Vector3 direction, taking into account the user arrow inputs the camera rotation
+	var direction = ($Camera_Controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+#	Rotate the character mesh so oriented towards the direction moving in relation to the camera
+	if input_dir != Vector2(0,0):
+		$MeshInstance3D.rotation_degrees.y = $Camera_Controller.rotation_degrees.y - rad_to_deg(input_dir.angle()) -90
+
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
